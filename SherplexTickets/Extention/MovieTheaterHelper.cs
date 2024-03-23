60f48fc680c1a4feefee516dcbd73a92999750d2
SherifPashov <sherifpashov2003@abv.bg>
2024-03-23 02:35:14 +0200

    Extracting CSS from HTML

---
+++ SherplexTickets/Extention/MovieTheaterHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace SherplexTickets.Extensions
+{
+    public static class MovieTheaterHelper
+    {
+        public static bool IsMovieTheaterOpen(DateTime openingTime, DateTime closingTime)
+        {
+            var now = DateTime.Now;
+
+            if (now.Hour < openingTime.Hour || now.Hour > closingTime.Hour)
+            {
+                return false;
+            }
+
+            if (now.Hour > openingTime.Hour && now.Hour < closingTime.Hour)
+            {
+                return true;
+            }
+
+            if (now.Hour == openingTime.Hour)
+            {
+                if (now.Minute >= openingTime.Minute)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (now.Minute <= closingTime.Minute)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
+
